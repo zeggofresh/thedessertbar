315253044d149ba963720911396fe5a53356f2d1
zeggofresh <zeggofresh@gmail.com>
2026-01-07 19:37:40 +0530

    dsgdg

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import { HashRouter } from 'react-router-dom' // <-- use HashRouter for GitHub Pages
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </StrictMode>
 )
